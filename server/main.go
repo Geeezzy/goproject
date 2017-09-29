e75835949ea3926213a9ef693820fa87724b1019
Geeezzy <Trakir4ik@gmail.com>
2017-09-30 00:39:43 +0300

    add autorization

--- server/main.go
+++ server/main.go
@@ -11,11 +11,12 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"context"
 	"fmt"
-	//"io"
 	"encoding/json"
+	"encoding/base64"
 	"github.com/dgrijalva/jwt-go"
 	"time"
 	"github.com/auth0/go-jwt-middleware"
+	"strings"
 )
 
 type Project struct {
@@ -25,6 +26,8 @@ type Project struct {
 const PORT string = ":8080"
 
 func main(){
+
+
 	r := mux.NewRouter()
 
 	r.Handle("/containers/create", jwtMiddleware.Handler(CreateCon)).Methods("POST")
@@ -33,14 +36,16 @@ func main(){
 	r.Handle("/containers/{id}/delete", jwtMiddleware.Handler(DeleteCon)).Methods("DELETE")
 	r.Handle("/containers/{id}", jwtMiddleware.Handler(GetInfCon)).Methods("GET")
 	r.Handle("/containers", jwtMiddleware.Handler(GetListCon)).Methods("GET")
+	r.Handle("/get-token", GetTokenHandler).Methods("POST")
 
-	r.Handle("/get-token", GetTokenHandler).Methods("GET")
 	log.Println("Server up and run on port " + PORT)
 	log.Fatal(http.ListenAndServe(PORT, handlers.LoggingHandler(os.Stdout, r)))
 }
 
+// Глобальный секретный ключ
 var mySigningKey = []byte("secret")
 
+//Прослойка, которая будет выполнять проверку нашего токена
 var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
 	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
 		return mySigningKey, nil
@@ -48,24 +53,43 @@ var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
 	SigningMethod: jwt.SigningMethodHS256,
 })
 
+//Роутер, который генерирует новый JWT и аутентификация пользователя
 var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-	// Создаем новый токен
-	token := jwt.New(jwt.SigningMethodHS256)
-	claims := token.Claims.(jwt.MapClaims)
-	
-	// Устанавливаем набор параметров для токена
-	claims["admin"] = true
-	claims["name"] = "Ado Kukic"
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 
-	// Подписываем токен нашим секретным ключем
-	tokenString, _ := token.SignedString(mySigningKey)
+	auth := strings.SplitN(r.Header["Authorization"][0], " ", 2)
+	if len(auth) != 2 || auth[0] != "Basic" {
+		http.Error(w, "bad syntax", http.StatusBadRequest)
+		return
+	}
 
-	// Отдаем токен клиенту
-	w.Write([]byte(tokenString))
+	payload, _ := base64.StdEncoding.DecodeString(auth[1])
+	pair := strings.SplitN(string(payload), ":", 2)
+
+	if Validate(pair[0], pair[1]) {
+		// Создаем новый токен
+		token := jwt.New(jwt.SigningMethodHS256)
+		claims := token.Claims.(jwt.MapClaims)
+
+		// Устанавливаем набор параметров для токена
+		claims["admin"] = true
+		claims["name"] = "klyov"
+		claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+		// Подписываем токен нашим секретным ключем
+		tokenString, err := token.SignedString(mySigningKey)
+		if err != nil {
+			panic(err)
+		}
+		// Отдаем токен клиенту
+		w.Write([]byte(tokenString))
+		w.WriteHeader(http.StatusOK)
+	} else{
+		w.WriteHeader(http.StatusUnauthorized)
+	}
 
 })
 
+//Роутер, создающий контейнер
 var CreateCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	project := Project{}
@@ -94,6 +118,7 @@ var CreateCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 })
 
+//Роутер, запускающий контейнер
 var RunCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Path[len("/containers/"):len("/containers/") + 12]
 	ctx := context.Background()
@@ -107,6 +132,7 @@ var RunCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 })
 
+//Роутер, останавливающий контейнер
 var StopCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Path[len("/containers/"):len("/containers/") + 12]
 	ctx := context.Background()
@@ -124,6 +150,7 @@ var StopCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 })
 
+//Роутер, удаляющий контейнер
 var DeleteCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Path[len("/containers/"):len("/containers/") + 12]
 	ctx := context.Background()
@@ -141,6 +168,7 @@ var DeleteCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 })
 
+//Роутер, который выдает информацию по контейнеру
 var GetInfCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Path[len("/containers/"):len("/containers/") + 12]
 	ctx := context.Background()
@@ -154,12 +182,16 @@ var GetInfCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	productsJson, _ := json.Marshal(inf)
+	productsJson, err := json.Marshal(inf)
+	if	err != nil {
+		panic(err)
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(productsJson)
 })
 
+//Роутер, который выдает список запущенных контейнеров
 var GetListCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	cli, err := client.NewEnvClient()
 	if err != nil {
@@ -178,4 +210,10 @@ var GetListCon = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 })
 
-
+//Проверка введеных данных
+func Validate(username, password string) bool {
+	if username == "Geeezzy" && password == "Trapa35" {
+		return true
+	}
+	return false
+}
